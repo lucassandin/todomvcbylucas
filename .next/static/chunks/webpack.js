e1145c198bbd0f5ab37d90133f35bdf3a9cc154f
Lucas Sandin <lucas@Lucass-MacBook-Pro.local>
2020-08-17 13:57:05 -0300

    updated package json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9eceafbdf1a7a1746095";
+/******/ 	var hotCurrentHash = "518f76cf36040f5e25c3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
